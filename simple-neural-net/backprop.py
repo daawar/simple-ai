943d3eacc3c92c3b2fd91680129475247c37e2a8
Daawar Khan <daawar@gmail.com>
2026-07-07 11:29:36 -0700

    intial commit

---
+++ simple-neural-net/backprop.py
@@ -0,0 +1,70 @@
+
+
+class Value:
+    def __init__(self, data, _children = (), _op = ''):
+        self.data = data
+        self.grad = 0.0
+        
+        self._backward = lambda : None ##TODO understand this syntax and use of this assignment
+        self._prev = set(_children)
+        self._op = _op
+
+    def __repr__(self):
+        return f"Value({self.data})"
+    
+    def __add__(self, other):
+        assert isinstance(other, Value) or isinstance(other, (int, float)), "Unsupported type for add operations- should be either int or float"
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        #calculate gradients in backward propagation
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad            
+
+        out._backward = _backward ##TODO understand this lamba assignment
+        return out
+    
+    def __mul__(self, other):
+        assert isinstance(other, Value) or isinstance(other, (int, float)), "Unsupported type for multiply operations- should be either int or float"
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            # w = u * v
+            # L(w)
+            # dL/du = dL/dw * dw/du (chain rule of multiplication)
+            self.grad += out.grad * other.data #Chain rule - upstream gradient wrt to Loss fucntion calculated so far and then Local gradient wrt current operation (d)
+            other.grad += out.grad * self.data
+
+        out._backward = _backward ##TODO understand this lamba assignment
+        return out
+
+    # mimics the backpropagation function "backward" from pytorch library
+    def backward(self):
+        topo = []
+        visited = set() # ALERT-- Using visited = () gave error about not able to add to tuple
+        def _toposort(v):
+            if v not in visited:
+                visited.add(v)
+                for u in v._prev:
+                    _toposort(u)
+                topo.append(v)
+        
+        ### ALERT - Missing this step was not showing any gradients for any nodes ####
+        _toposort(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+        
+
+#test code below
+a = Value(1.0)
+b = Value(2.0)
+c = a + b #(a, b) -+-> c
+d = Value(4.0)
+e = d * c * 0.5 #(d, c) -*-> e
+print(e)
+
+e.backward
---
+++ simple-neural-net/graph.py
@@ -0,0 +1,48 @@
+
+from backprop import Value
+from graphviz import Digraph
+
+def build_nodes_graph(root):
+    nodes, edges = set(), set()
+
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for u in v._prev:
+                edges.add((u, v))
+                build(u)
+    build(root)
+    return nodes, edges
+
+
+def build_display_graph(root):
+    nodes, edges = build_nodes_graph(root)
+    g = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
+
+    for node in nodes:
+        g.node(name=str(id(node)), label="{data %.4f | grad %.4f}" % (node.data, node.grad), shape = 'record')
+
+        if node._op:
+            g.node(name=str(id(node)) + node._op, label=node._op)
+            g.edge(str(id(node)) + node._op, str(id(node)))
+
+    for n1, n2 in edges:
+        g.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return g
+
+
+#test nodes builder
+
+a = Value(1.0)
+b = Value(2.0)
+c = a + b #(a, b) -+-> c
+d = Value(4.0)
+e = d * c * 0.5 #(d, c) -*-> e
+print(e)
+
+e.backward()
+
+g = build_display_graph(e)
+g.render('gout')
+
