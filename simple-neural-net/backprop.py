6603e58bcdff90bcb08a99ccc5674b34defa0cb3
Daawar Khan <daawar@gmail.com>
2026-07-12 22:02:47 -0700

    added activation function tanh

--- simple-neural-net/backprop.py
+++ simple-neural-net/backprop.py
@@ -1,70 +1,99 @@
+import math
 
 
 class Value:
-    def __init__(self, data, _children = (), _op = ''):
+    def __init__(self, data, _children=(), _op=""):
         self.data = data
         self.grad = 0.0
-        
-        self._backward = lambda : None ##TODO understand this syntax and use of this assignment
+
+        self._backward = lambda: (
+            None
+        )  ##TODO understand this syntax and use of this assignment
         self._prev = set(_children)
         self._op = _op
 
     def __repr__(self):
         return f"Value({self.data})"
-    
-    def __add__(self, other):
-        assert isinstance(other, Value) or isinstance(other, (int, float)), "Unsupported type for add operations- should be either int or float"
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other), '+')
 
-        #calculate gradients in backward propagation
+    def __add__(self, other):
+        assert isinstance(other, Value) or isinstance(other, (int, float)), (
+            "Unsupported type for add operations- should be either int or float"
+        )
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), "+")
+
+        # calculate gradients in backward propagation
         def _backward():
             self.grad += out.grad
-            other.grad += out.grad            
+            other.grad += out.grad
 
-        out._backward = _backward ##TODO understand this lamba assignment
+        out._backward = _backward  ##TODO understand this lamba assignment
         return out
-    
+
     def __mul__(self, other):
-        assert isinstance(other, Value) or isinstance(other, (int, float)), "Unsupported type for multiply operations- should be either int or float"
+        assert isinstance(other, Value) or isinstance(other, (int, float)), (
+            "Unsupported type for multiply operations- should be either int or float"
+        )
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data, (self, other), '*')
+        out = Value(self.data * other.data, (self, other), "*")
 
         def _backward():
             # w = u * v
             # L(w)
             # dL/du = dL/dw * dw/du (chain rule of multiplication)
-            self.grad += out.grad * other.data #Chain rule - upstream gradient wrt to Loss fucntion calculated so far and then Local gradient wrt current operation (d)
+            self.grad += (
+                out.grad * other.data
+            )  # Chain rule - upstream gradient wrt to Loss fucntion calculated so far and then Local gradient wrt current operation (d)
             other.grad += out.grad * self.data
 
-        out._backward = _backward ##TODO understand this lamba assignment
+        out._backward = _backward  ##TODO understand this lamba assignment
+        return out
+
+    # def __pow__(self, other):
+    #     assert isinstance(other, (int, float)), 'only int or float exponents allowed'
+    #     out = Value(self.data ** other, (self, other))
+
+    #     return out
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(x) - math.exp(-x)) / (math.exp(x) + math.exp(-x))
+        out = Value(t, (self,), "tanh")
+
+        def _backward():
+            self.grad = (1 - t**2) * out.grad
+
+        out._backward = _backward
         return out
 
     # mimics the backpropagation function "backward" from pytorch library
     def backward(self):
         topo = []
-        visited = set() # ALERT-- Using visited = () gave error about not able to add to tuple
+        visited = (
+            set()
+        )  # ALERT-- Using visited = () gave error about not able to add to tuple
+
         def _toposort(v):
             if v not in visited:
                 visited.add(v)
                 for u in v._prev:
                     _toposort(u)
                 topo.append(v)
-        
+
         ### ALERT - Missing this step was not showing any gradients for any nodes ####
         _toposort(self)
 
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
-        
 
-#test code below
+
+# test code below
 a = Value(1.0)
 b = Value(2.0)
-c = a + b #(a, b) -+-> c
+c = a + b  # (a, b) -+-> c
 d = Value(4.0)
-e = d * c * 0.5 #(d, c) -*-> e
+e = d * c * 0.5  # (d, c) -*-> e
 print(e)
 
 e.backward
--- simple-neural-net/graph.py
+++ simple-neural-net/graph.py
@@ -1,6 +1,6 @@
-
 from backprop import Value
 from graphviz import Digraph
+
 
 def build_nodes_graph(root):
     nodes, edges = set(), set()
@@ -11,16 +11,21 @@ def build_nodes_graph(root):
             for u in v._prev:
                 edges.add((u, v))
                 build(u)
+
     build(root)
     return nodes, edges
 
 
 def build_display_graph(root):
     nodes, edges = build_nodes_graph(root)
-    g = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
+    g = Digraph(format="svg", graph_attr={"rankdir": "LR"})
 
     for node in nodes:
-        g.node(name=str(id(node)), label="{data %.4f | grad %.4f}" % (node.data, node.grad), shape = 'record')
+        g.node(
+            name=str(id(node)),
+            label="{data %.4f | grad %.4f}" % (node.data, node.grad),
+            shape="record",
+        )
 
         if node._op:
             g.node(name=str(id(node)) + node._op, label=node._op)
@@ -28,21 +33,40 @@ def build_display_graph(root):
 
     for n1, n2 in edges:
         g.edge(str(id(n1)), str(id(n2)) + n2._op)
-    
+
     return g
 
 
-#test nodes builder
+# test nodes builder
 
 a = Value(1.0)
 b = Value(2.0)
-c = a + b #(a, b) -+-> c
+c = a + b  # (a, b) -+-> c
 d = Value(4.0)
-e = d * c * 0.5 #(d, c) -*-> e
+e = d * c * 0.5  # (d, c) -*-> e
 print(e)
 
 e.backward()
 
-g = build_display_graph(e)
-g.render('gout')
+# g = build_display_graph(e)
+# g.render("gout")
 
+# neuron mimic - using activation function tanh
+w1 = Value(0.5)
+x1 = Value(2.0)
+w2 = Value(0.3)
+x2 = Value(5.0)
+b = Value(0.634846)
+
+f = w1 * x1 + w2 * x2 + b
+print(f)
+
+# use activation function
+h = f.tanh()
+print(h)
+
+# backpropagate updating gradients on the way back
+h.backward()
+
+g = build_display_graph(h)
+g.render("gout")
